ee7f344857f5303815d9ad69b02426b6bff79e5b
tterrag1098 <tterrag1098@gmail.com>
2014-10-01 16:45:08 -0400

    Use proper constants for StringUtils

--- src/main/java/fluxedtrinkets/api/StringUtils.java
+++ src/main/java/fluxedtrinkets/api/StringUtils.java
@@ -2,6 +2,7 @@ package fluxedtrinkets.api;
 
 import java.text.DecimalFormat;
 
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
 
 import org.lwjgl.input.Keyboard;
@@ -12,29 +13,29 @@ public final class StringUtils {
 
     private static DecimalFormat formatter = new DecimalFormat("###,###");
 
-    public static final String BLACK = (char) 167 + "0";
-    public static final String BLUE = (char) 167 + "1";
-    public static final String GREEN = (char) 167 + "2";
-    public static final String TEAL = (char) 167 + "3";
-    public static final String RED = (char) 167 + "4";
-    public static final String PURPLE = (char) 167 + "5";
-    public static final String ORANGE = (char) 167 + "6";
-    public static final String LIGHT_GRAY = (char) 167 + "7";
-    public static final String GRAY = (char) 167 + "8";
-    public static final String LIGHT_BLUE = (char) 167 + "9";
-    public static final String BRIGHT_GREEN = (char) 167 + "a";
-    public static final String BRIGHT_BLUE = (char) 167 + "b";
-    public static final String LIGHT_RED = (char) 167 + "c";
-    public static final String PINK = (char) 167 + "d";
-    public static final String YELLOW = (char) 167 + "e";
-    public static final String WHITE = (char) 167 + "f";
+    public static final String BLACK = EnumChatFormatting.BLACK.toString();
+    public static final String BLUE = EnumChatFormatting.BLUE.toString();
+    public static final String GREEN = EnumChatFormatting.GREEN.toString();
+    public static final String TEAL = EnumChatFormatting.AQUA.toString();
+    public static final String RED = EnumChatFormatting.DARK_RED.toString();
+    public static final String PURPLE = EnumChatFormatting.DARK_PURPLE.toString();
+    public static final String ORANGE = EnumChatFormatting.GOLD.toString();
+    public static final String LIGHT_GRAY = EnumChatFormatting.GRAY.toString();
+    public static final String GRAY = EnumChatFormatting.DARK_GRAY.toString();
+    public static final String LIGHT_BLUE = EnumChatFormatting.DARK_BLUE.toString();
+    public static final String BRIGHT_GREEN = EnumChatFormatting.GREEN.toString();
+    public static final String BRIGHT_BLUE = EnumChatFormatting.BLUE.toString();
+    public static final String LIGHT_RED = EnumChatFormatting.RED.toString();
+    public static final String PINK = EnumChatFormatting.LIGHT_PURPLE.toString();
+    public static final String YELLOW = EnumChatFormatting.YELLOW.toString();
+    public static final String WHITE = EnumChatFormatting.WHITE.toString();
 
-    public static final String OBFUSCATED = (char) 167 + "k";
-    public static final String BOLD = (char) 167 + "l";
-    public static final String STRIKETHROUGH = (char) 167 + "m";
-    public static final String UNDERLINE = (char) 167 + "n";
-    public static final String ITALIC = (char) 167 + "o";
-    public static final String END = (char) 167 + "r";
+    public static final String OBFUSCATED = EnumChatFormatting.OBFUSCATED.toString();
+    public static final String BOLD = EnumChatFormatting.BOLD.toString();
+    public static final String STRIKETHROUGH = EnumChatFormatting.STRIKETHROUGH.toString();
+    public static final String UNDERLINE = EnumChatFormatting.UNDERLINE.toString();
+    public static final String ITALIC = EnumChatFormatting.ITALIC.toString();
+    public static final String END = EnumChatFormatting.RESET.toString();
 
     public static boolean isAltKeyDown() {
         return Keyboard.isKeyDown(Keyboard.KEY_LMENU) || Keyboard.isKeyDown(Keyboard.KEY_RMENU);
